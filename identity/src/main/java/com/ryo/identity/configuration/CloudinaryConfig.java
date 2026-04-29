c15dc26dd9908fb7b0569225a7023573e0681d30
tung <tung.bui2004lk@hcmut.edu.vn>
2026-04-29 11:34:54 +0700

    test: default env val in ci.yml

--- identity/src/main/java/com/ryo/identity/configuration/CloudinaryConfig.java
+++ identity/src/main/java/com/ryo/identity/configuration/CloudinaryConfig.java
@@ -11,7 +11,7 @@ import org.springframework.context.annotation.Configuration;
 public class CloudinaryConfig {
 
     @NonFinal
-    @Value("${cloudinary.name:name}")
+    @Value("${cloudinary.name:default}")
     String CLOUDINARY_CLOUD_NAME;
 
     @NonFinal
