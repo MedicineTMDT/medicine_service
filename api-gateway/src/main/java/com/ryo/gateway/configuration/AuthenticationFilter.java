7b174f7350e1b100bba19fb487fdf634a4c9c356
tung <tung.bui2004lk@hcmut.edu.vn>
2026-05-01 16:37:47 +0700

    feat: api gateway for another service

---
+++ api-gateway/src/main/java/com/ryo/gateway/configuration/AuthenticationFilter.java
@@ -0,0 +1,105 @@
+package com.ryo.gateway.configuration;
+
+import com.ryo.gateway.dto.ApiResponse;
+import com.ryo.gateway.service.IdentityService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.experimental.NonFinal;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@Slf4j
+public class AuthenticationFilter implements GlobalFilter, Ordered {
+    IdentityService identityService;
+    ObjectMapper objectMapper;
+
+    @NonFinal
+    private String[] publicEndpoint = {
+            "/identity/auth/.*",
+            "/identity/users/registration"
+    };
+
+    @NonFinal
+    @Value("${app.api-prefix}")
+    private String apiPrefix;
+
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        log.info("Enter authentication filter");
+
+        if(isPublicEndpoint(exchange.getRequest()))
+        {
+            return chain.filter(exchange);
+        }
+
+        // Authentication
+        // Get Token from Authorization Header
+        List<String> authHeader = exchange.getRequest().getHeaders().get(HttpHeaders.AUTHORIZATION);
+        assert authHeader != null;
+        if (authHeader.isEmpty()){
+            return unauthenticated(exchange.getResponse());
+        }
+
+        String token = authHeader.getFirst().replace("Bearer ", "");
+        log.info("Token: {}", token);
+
+        return identityService.introspect(token).flatMap(introspectResponseApiResponse -> {
+            if(introspectResponseApiResponse.getResult().isValid()){
+                return chain.filter(exchange);
+            }
+            else{
+                return unauthenticated(exchange.getResponse());
+            }
+        }).onErrorResume(throwable -> unauthenticated(exchange.getResponse()));
+    }
+
+    @Override
+    public int getOrder() {
+        return -1;
+    }
+
+    private boolean isPublicEndpoint(ServerHttpRequest request){
+        return Arrays.stream(publicEndpoint).anyMatch(s -> request.getURI().getPath().matches(
+                apiPrefix + s
+        ));
+    }
+
+    Mono<Void> unauthenticated(ServerHttpResponse response) {
+        ApiResponse<?> apiResponse = ApiResponse.builder()
+                .code(1401)
+                .message("Unauthenticated")
+                .build();
+        String body = null;
+        try {
+            body = objectMapper.writeValueAsString(apiResponse);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+        response.setStatusCode(HttpStatus.UNAUTHORIZED);
+        response.getHeaders().add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
+        return response.writeWith(
+                Mono.just(response.bufferFactory().wrap(body.getBytes()))
+        );
+    }
+}
---
+++ api-gateway/src/main/java/com/ryo/gateway/configuration/WebClientConfiguration.java
@@ -0,0 +1,33 @@
+package com.ryo.gateway.configuration;
+
+import com.ryo.gateway.repository.IdentityClient;
+import lombok.experimental.NonFinal;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.support.WebClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+@Configuration
+public class WebClientConfiguration {
+
+    @NonFinal
+    @Value("${services.identity.url}")
+    private String identityUrl;
+
+    @Bean
+    WebClient webClient(){
+        return WebClient.builder()
+                .baseUrl(identityUrl + "/identity")
+                .build();
+    }
+
+    @Bean
+    IdentityClient identityClient(WebClient webClient){
+        HttpServiceProxyFactory httpServiceProxyFactory = HttpServiceProxyFactory
+                .builderFor(WebClientAdapter.create(webClient)).build();
+
+        return httpServiceProxyFactory.createClient(IdentityClient.class);
+    }
+}
